6975c1ccd25b54209290eddc1cd5ff73ab65cc86
Esubaalew <esubalewchekol6@gmail.com>
2023-12-28 19:34:39 +0300

    made postgres as database

--- Enimar/settings.py
+++ Enimar/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Enimar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'enimar',
+        'USER': 'postgres',
+        'PASSWORD': 'esube',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
