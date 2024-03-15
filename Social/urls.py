174db46f70cf345594bd425b8b1719ecb726db5c
Esubaalew <esubalewchekol6@gmail.com>
2024-03-15 20:04:55 +0300

    Social app[sent message gone]: msg sent page changed by redirection to the conversation

--- Social/urls.py
+++ Social/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('post/create/', views.create_post, name='create_post'),
     path('message/<int:recipient_id>/', views.send_message, name='send_message'),
     path('inbox/', views.inbox, name='inbox'),
-    path('message/sent/', views.message_sent, name='message_sent'),
     path('conversation/<int:other_user_id>/', views.conversation, name='conversation'),
 
 ]
