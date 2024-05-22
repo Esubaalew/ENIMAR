ffc492b2c9c0140c001ebc4a3d625cb8904db816
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-05-22 09:06:50 +0300

    defined rest auth rules

--- Enimar/settings.py
+++ Enimar/settings.py
@@ -136,3 +136,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'Account.CustomUser'
 CORS_ORIGIN_ALLOW_ALL = True
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
