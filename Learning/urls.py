6e09ea236cf8dcb81d591e2ca5ba43f843070ace
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-06-04 23:38:47 +0300

    some cange

--- Learning/urls.py
+++ Learning/urls.py
@@ -24,12 +24,6 @@ router.register(r'course-photos', CoursePhotoViewSet, 'course-photos')
 router.register(r'course-videos', CourseVideoViewSet, 'course-videos')
 
 urlpatterns = [
-    # path('courses/list', views.course_list, name='course_list'),
-    # path('courses/create/', views.create_course, name='create_course'),
-    # path('courses/<int:pk>', CourseDetailView.as_view(), name='course_detail'),
-    # path('courses/enroll/<int:pk>', views.enroll, name='enroll'),
-    # path('payment/check/<int:pk>', views.payment, name='check'),
-    # path('course/attend/<int:pk>', views.attend, name='attend'),
     path('', include(router.urls)),
 
 ]
