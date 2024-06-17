1eaa102d8896ffc75fc299b25e8c331417533c68
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-06-17 18:24:42 +0300

    view payments

--- payments/urls.py
+++ payments/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('initialize/', PaymentInitializeView.as_view(), name='api_payment_initialize'),
     path('callback/', PaymentCallbackView.as_view(), name='payment_callback'),
     path('teacher/<int:teacher_id>/', PaymentsByTeacherView.as_view(), name='payments-by-teacher'),
+
 ]
